16b8e79705ea0d8308b2addc627701165d8fd713
mozes-lol <johnmarkgarcia164@gmail.com>
2023-02-05 13:32:42 +0800

    Reads From Menu.csv (More reliable)

--- CosCafe POS/CosCafe POS/AddOrderEntry.cs
+++ CosCafe POS/CosCafe POS/AddOrderEntry.cs
@@ -24,17 +24,28 @@ namespace CosCafe_POS
             // loads Menu.csv
             using (var streamReader = new StreamReader(@"\Menu.csv"))
             {
+                // reads Menu.csv
                 try
                 {
-                    // reads Menu.csv
-                    Debug.Print(streamReader.ReadToEnd());
+                    // as long as the next line is not null...
+                    while (streamReader.Peek() != -1)
+                    {
+                        // takes a line a splits it into two parts: The ORDER NAME and the COST
+                        string[] menuEntry = streamReader.ReadLine().Split(',');
+                        // reads order name only (avoids reading the cost)
+                        for(int studCount = 0; studCount < 1; studCount++)
+                        {
+                            // adds those order names to the combo box
+                            comboBox_chooseOrder.Items.Add(menuEntry[0]);
+                        }
+                    }
                 }
                 catch (Exception ex)
                 {
+                    // in case if it doesn't work
                     Debug.Print("You monkey! Look what have you done: " + ex);
                 }
             }
-            using (var streamWriter = new StreamWriter(@"\Test.csv"));
         }
 
         private void AddOrderEntry_FormClosed(object sender, FormClosedEventArgs e)
