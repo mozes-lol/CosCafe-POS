7c8ba2ed154972ded5a3500429f9581a06d06d4d
mozes-lol <johnmarkgarcia164@gmail.com>
2023-02-06 16:32:02 +0800

    ajsehtiwehtnoiawehnaivow

--- CosCafe POS/CosCafe POS/Main.Designer.cs
+++ CosCafe POS/CosCafe POS/Main.Designer.cs
@@ -40,6 +40,7 @@ namespace CosCafe_POS
             this.label_totalCost = new System.Windows.Forms.Label();
             this.label_totalCostNumber = new System.Windows.Forms.Label();
             this.panel_customerOrderList = new System.Windows.Forms.Panel();
+            this.button_deleteOrderList = new System.Windows.Forms.Button();
             this.numericUpDown_cost = new System.Windows.Forms.NumericUpDown();
             this.numericUpDown_Quantity = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
@@ -56,10 +57,10 @@ namespace CosCafe_POS
             // 
             // button_addOrderDetails
             // 
-            this.button_addOrderDetails.Location = new System.Drawing.Point(2, 216);
+            this.button_addOrderDetails.Location = new System.Drawing.Point(2, 293);
             this.button_addOrderDetails.Margin = new System.Windows.Forms.Padding(2);
             this.button_addOrderDetails.Name = "button_addOrderDetails";
-            this.button_addOrderDetails.Size = new System.Drawing.Size(153, 45);
+            this.button_addOrderDetails.Size = new System.Drawing.Size(47, 10);
             this.button_addOrderDetails.TabIndex = 0;
             this.button_addOrderDetails.Text = "Add Order";
             this.button_addOrderDetails.UseVisualStyleBackColor = true;
@@ -67,14 +68,16 @@ namespace CosCafe_POS
             // 
             // button_confirmCustomerOrderList
             // 
-            this.button_confirmCustomerOrderList.BackColor = System.Drawing.Color.MistyRose;
-            this.button_confirmCustomerOrderList.Location = new System.Drawing.Point(12, 120);
+            this.button_confirmCustomerOrderList.BackColor = System.Drawing.Color.Lime;
+            this.button_confirmCustomerOrderList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_confirmCustomerOrderList.Location = new System.Drawing.Point(14, 68);
             this.button_confirmCustomerOrderList.Margin = new System.Windows.Forms.Padding(2);
             this.button_confirmCustomerOrderList.Name = "button_confirmCustomerOrderList";
             this.button_confirmCustomerOrderList.Size = new System.Drawing.Size(131, 45);
             this.button_confirmCustomerOrderList.TabIndex = 1;
             this.button_confirmCustomerOrderList.Text = "Confirm Order List";
             this.button_confirmCustomerOrderList.UseVisualStyleBackColor = false;
+            this.button_confirmCustomerOrderList.Click += new System.EventHandler(this.button_confirmCustomerOrderList_Click);
             // 
             // listBox_orderName
             // 
@@ -85,9 +88,11 @@ namespace CosCafe_POS
             this.listBox_orderName.Size = new System.Drawing.Size(152, 238);
             this.listBox_orderName.TabIndex = 5;
             this.listBox_orderName.SelectedIndexChanged += new System.EventHandler(this.listBox_orderName_SelectedIndexChanged);
+            this.listBox_orderName.DoubleClick += new System.EventHandler(this.listBox_orderName_DoubleClick);
             // 
             // listBox_quantity
             // 
+            this.listBox_quantity.Enabled = false;
             this.listBox_quantity.FormattingEnabled = true;
             this.listBox_quantity.Location = new System.Drawing.Point(315, 23);
             this.listBox_quantity.Margin = new System.Windows.Forms.Padding(2);
@@ -97,6 +102,7 @@ namespace CosCafe_POS
             // 
             // listBox_orderCost
             // 
+            this.listBox_orderCost.Enabled = false;
             this.listBox_orderCost.FormattingEnabled = true;
             this.listBox_orderCost.Location = new System.Drawing.Point(356, 23);
             this.listBox_orderCost.Margin = new System.Windows.Forms.Padding(2);
@@ -158,6 +164,7 @@ namespace CosCafe_POS
             // panel_customerOrderList
             // 
             this.panel_customerOrderList.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel_customerOrderList.Controls.Add(this.button_deleteOrderList);
             this.panel_customerOrderList.Controls.Add(this.button_addOrderDetails);
             this.panel_customerOrderList.Controls.Add(this.listBox_orderName);
             this.panel_customerOrderList.Controls.Add(this.label_orderName);
@@ -173,18 +180,32 @@ namespace CosCafe_POS
             this.panel_customerOrderList.Size = new System.Drawing.Size(445, 307);
             this.panel_customerOrderList.TabIndex = 13;
             // 
+            // button_deleteOrderList
+            // 
+            this.button_deleteOrderList.BackColor = System.Drawing.Color.Red;
+            this.button_deleteOrderList.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_deleteOrderList.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button_deleteOrderList.Location = new System.Drawing.Point(14, 175);
+            this.button_deleteOrderList.Margin = new System.Windows.Forms.Padding(2);
+            this.button_deleteOrderList.Name = "button_deleteOrderList";
+            this.button_deleteOrderList.Size = new System.Drawing.Size(131, 45);
+            this.button_deleteOrderList.TabIndex = 13;
+            this.button_deleteOrderList.Text = "Delete Order List";
+            this.button_deleteOrderList.UseVisualStyleBackColor = false;
+            this.button_deleteOrderList.Click += new System.EventHandler(this.button_deleteOrderList_Click);
+            // 
             // numericUpDown_cost
             // 
             this.numericUpDown_cost.Location = new System.Drawing.Point(73, 108);
             this.numericUpDown_cost.Name = "numericUpDown_cost";
-            this.numericUpDown_cost.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDown_cost.Size = new System.Drawing.Size(224, 20);
             this.numericUpDown_cost.TabIndex = 20;
             // 
             // numericUpDown_Quantity
             // 
             this.numericUpDown_Quantity.Location = new System.Drawing.Point(73, 135);
             this.numericUpDown_Quantity.Name = "numericUpDown_Quantity";
-            this.numericUpDown_Quantity.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDown_Quantity.Size = new System.Drawing.Size(224, 20);
             this.numericUpDown_Quantity.TabIndex = 19;
             // 
             // label1
@@ -198,12 +219,13 @@ namespace CosCafe_POS
             // 
             // button_enterOrderEntry
             // 
-            this.button_enterOrderEntry.Location = new System.Drawing.Point(15, 175);
+            this.button_enterOrderEntry.Location = new System.Drawing.Point(60, 175);
             this.button_enterOrderEntry.Name = "button_enterOrderEntry";
             this.button_enterOrderEntry.Size = new System.Drawing.Size(202, 52);
             this.button_enterOrderEntry.TabIndex = 17;
             this.button_enterOrderEntry.Text = "Enter Order";
             this.button_enterOrderEntry.UseVisualStyleBackColor = true;
+            this.button_enterOrderEntry.Click += new System.EventHandler(this.button_enterOrderEntry_Click);
             // 
             // label_cost
             // 
@@ -228,7 +250,7 @@ namespace CosCafe_POS
             this.comboBox_chooseOrder.FormattingEnabled = true;
             this.comboBox_chooseOrder.Location = new System.Drawing.Point(72, 81);
             this.comboBox_chooseOrder.Name = "comboBox_chooseOrder";
-            this.comboBox_chooseOrder.Size = new System.Drawing.Size(151, 21);
+            this.comboBox_chooseOrder.Size = new System.Drawing.Size(225, 21);
             this.comboBox_chooseOrder.TabIndex = 14;
             this.comboBox_chooseOrder.SelectedIndexChanged += new System.EventHandler(this.comboBox_chooseOrder_SelectedIndexChanged);
             // 
@@ -244,14 +266,14 @@ namespace CosCafe_POS
             this.panel_addOrderEntry.Controls.Add(this.button_enterOrderEntry);
             this.panel_addOrderEntry.Location = new System.Drawing.Point(463, 12);
             this.panel_addOrderEntry.Name = "panel_addOrderEntry";
-            this.panel_addOrderEntry.Size = new System.Drawing.Size(244, 307);
+            this.panel_addOrderEntry.Size = new System.Drawing.Size(324, 307);
             this.panel_addOrderEntry.TabIndex = 21;
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(719, 331);
+            this.ClientSize = new System.Drawing.Size(799, 331);
             this.Controls.Add(this.panel_addOrderEntry);
             this.Controls.Add(this.panel_customerOrderList);
             this.Margin = new System.Windows.Forms.Padding(2);
@@ -279,7 +301,6 @@ namespace CosCafe_POS
         private System.Windows.Forms.Label label_quantity;
         private System.Windows.Forms.Label label_orderCost;
         private System.Windows.Forms.Label label_totalCost;
-        private System.Windows.Forms.Label label_totalCostNumber;
         private System.Windows.Forms.Panel panel_customerOrderList;
         private System.Windows.Forms.NumericUpDown numericUpDown_cost;
         private System.Windows.Forms.NumericUpDown numericUpDown_Quantity;
@@ -289,6 +310,8 @@ namespace CosCafe_POS
         private System.Windows.Forms.Label label_chooseOrder;
         private System.Windows.Forms.ComboBox comboBox_chooseOrder;
         private System.Windows.Forms.Panel panel_addOrderEntry;
+        public System.Windows.Forms.Label label_totalCostNumber;
+        private System.Windows.Forms.Button button_deleteOrderList;
     }
 }
 
--- CosCafe POS/CosCafe POS/Main.cs
+++ CosCafe POS/CosCafe POS/Main.cs
@@ -14,6 +14,8 @@ namespace CosCafe_POS
 {
     public partial class Main : Form
     {
+        float totalCost;
+
         public Main()
         {
             InitializeComponent();
@@ -89,6 +91,75 @@ namespace CosCafe_POS
                     Debug.Print("You monkey! Look what have you done: " + ex);
                 }
             }
+        }
+
+        private void button_enterOrderEntry_Click(object sender, EventArgs e)
+        {
+            listBox_orderName.Items.Add(comboBox_chooseOrder.SelectedItem);
+            listBox_quantity.Items.Add(numericUpDown_Quantity.Value);
+            // If the user choose an order with a quantity of 2 or more, we multiply the cost by that quantity.
+            float multipliedCost = (float)numericUpDown_Quantity.Value * (float)numericUpDown_cost.Value;
+            listBox_orderCost.Items.Add(multipliedCost);
+            totalCost += multipliedCost;
+            label_totalCostNumber.Text = totalCost.ToString();
+        }
+
+        private void listBox_orderName_DoubleClick(object sender, EventArgs e)
+        {
+            try
+            {
+                int selectedIndex = listBox_orderName.SelectedIndex;
+                // removing an item in the COL
+                listBox_orderName.Items.RemoveAt(listBox_orderName.SelectedIndex);
+                listBox_quantity.Items.RemoveAt(selectedIndex);
+                // Since we're removing the cost of the order, we deduct it to the total cost.
+                listBox_orderCost.SelectedIndex = selectedIndex;
+                float deduct = (float)listBox_orderCost.SelectedItem;
+                totalCost -= deduct;
+                listBox_orderCost.Items.RemoveAt(selectedIndex);
+                label_totalCostNumber.Text = totalCost.ToString();
+            }
+            catch (Exception ex)
+            {
+                // in case if it doesn't work
+                Debug.Print("You monkey! Look what have you done: " + ex);
+            }
+        }
+
+        private void clearOrderList()
+        {
+            // removes everything in the COL
+            listBox_orderCost.Items.Clear();
+            listBox_quantity.Items.Clear();
+            listBox_orderName.Items.Clear();
+            totalCost = 0;
+            label_totalCostNumber.Text = totalCost.ToString();
+        }
+
+        private void button_deleteOrderList_Click(object sender, EventArgs e)
+        {
+            clearOrderList();
+        }
+
+        private void button_confirmCustomerOrderList_Click(object sender, EventArgs e)
+        {
+            // The ROS will display the following:
+            // - Order Name
+            // - Quantity
+            // - Cost (Multiplied by Quantity)
+            // (P.S: Calculations and counting of data can all be handled on Excel. So don't worry about data organization.)
+            StreamWriter streamWriter = new StreamWriter(@"\RecordOfSales.csv", true);
+            for (int line = 0; line < listBox_orderName.Items.Count; line++)
+            {
+                streamWriter.WriteLine
+                    (
+                        listBox_orderName.Items[line].ToString() + "," +
+                        listBox_quantity.Items[line].ToString() + "," +
+                        listBox_orderCost.Items[line].ToString()
+                    );
+            }
+            streamWriter.Close();
+            clearOrderList();
         }
     }
 }
